94eaffc22db41d2785834d111d345d995d4ee2d9
Valentin Ackva <valentin.ackva@insone.de>
2024-05-05 18:01:49 +0200

    update SofaReader class - added sofa file loading via binary data

---
+++ source/dsp/SofaReader.cpp
@@ -0,0 +1,41 @@
+#include "SofaReader.h"
+
+SofaReader::~SofaReader() {
+    mysofa_close(*sofa);
+    sofa.reset();
+}
+
+void SofaReader::prepare(double samplerate) {
+    if (sofa != nullptr){
+        sofa.reset();
+    }
+    int err;
+    sofa = std::make_unique<MYSOFA_EASY*>(mysofa_open_data(BinaryData::pp1_HRIRs_measured_sofa, BinaryData::pp1_HRIRs_measured_sofaSize, static_cast<float>(samplerate), &ir_length, &err));
+    switch (err) {
+        case MYSOFA_OK:
+            std::cout << "Successfully loaded Sofa File" << std::endl;
+            std::cout << "Length of IRs: " << ir_length << std::endl;
+            break;
+        default:
+            std::cout << "Error while loading Sofa File" << std::endl;
+            return;
+    }
+}
+
+int SofaReader::get_ir_length() {
+    return ir_length;
+}
+
+void SofaReader::get_hrirs(AudioBuffer<float> &buffer, float azim, float elev, float dist) {
+    auto leftIR = buffer.getWritePointer(0);
+    auto rightIR = buffer.getWritePointer(1);
+    float leftDelay;
+    float rightDelay;
+    // convert coordinates to xyz
+    coordinate_buffer[0] = azim;
+    coordinate_buffer[1] = elev;
+    coordinate_buffer[2] = dist;
+    mysofa_s2c((float *) &coordinate_buffer);
+
+    mysofa_getfilter_float(*sofa, coordinate_buffer[0], coordinate_buffer[1], coordinate_buffer[2], leftIR, rightIR, &leftDelay, &rightDelay);
+}
--- source/dsp/SofaReader.h
+++ source/dsp/SofaReader.h
@@ -10,50 +10,13 @@
 
 class SofaReader {
 public:
-    SofaReader() {
-        // SOFA_TEST_FILE_PATH defined in CMakeLists.txt - TODO load as Binary if possible    
-    }
+    SofaReader() = default;
+    ~SofaReader();
 
-    ~SofaReader() {
-        mysofa_close(*sofa);
-        sofa.reset();
-    }
+    void prepare(double samplerate);
 
-    void prepare(double samplerate){
-        if (sofa != nullptr){
-            sofa.reset();
-        }
-        int err;
-        std::cout << SOFA_TEST_FILE_PATH << std::endl;
-        sofa = std::make_unique<MYSOFA_EASY*>(mysofa_open(SOFA_TEST_FILE_PATH, static_cast<float>(samplerate), &ir_length, &err));
-        switch (err) {
-            case MYSOFA_OK:
-                std::cout << "Successfully loaded Sofa File" << std::endl;
-                std::cout << "Length of IRs: " << ir_length << std::endl;
-                break;
-            default:
-                std::cout << "Error while loading Sofa File" << std::endl;
-                return;
-        }
-    }
-
-    int get_ir_length(){
-        return ir_length;
-    }
-
-    void get_hrirs(juce::AudioBuffer<float>& buffer, float azim, float elev, float dist){
-        auto leftIR = buffer.getWritePointer(0);
-        auto rightIR = buffer.getWritePointer(1);
-        float leftDelay;
-        float rightDelay;
-        // convert coordinates to xyz
-        coordinate_buffer[0] = azim;
-        coordinate_buffer[1] = elev;
-        coordinate_buffer[2] = dist;
-        mysofa_s2c((float *) &coordinate_buffer);
-
-        mysofa_getfilter_float(*sofa, coordinate_buffer[0], coordinate_buffer[1], coordinate_buffer[2], leftIR, rightIR, &leftDelay, &rightDelay);
-    }
+    int get_ir_length();
+    void get_hrirs(juce::AudioBuffer<float>& buffer, float azim, float elev, float dist);
 
 private:
     int ir_length;
