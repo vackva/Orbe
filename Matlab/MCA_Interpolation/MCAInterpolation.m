ef4d8b6478e05fd29af6969a4c0f1015fedf3b9f
mcht67 <115042091+mcht67@users.noreply.github.com>
2024-06-17 20:23:11 +0200

    adds sh and timealign interpolations

--- Matlab/MCA_Interpolation/MCAInterpolation.m
+++ Matlab/MCA_Interpolation/MCAInterpolation.m
@@ -7,16 +7,29 @@ sparseHRIRdataset_SOFA = SOFAload('pp2_HRIRs_measured.sofa');
 hrtfDataset = supdeq_sofa2hrtf(sparseHRIRdataset_SOFA, N);
 
 % MCA Interpolation
-interpHRTF_mca = supdeq_interpHRTF(hrtfDataset, denseSamplingGrid,'SUpDEq','SH');
+headRadius = 0.0875;
+
+interpHRTF_sh = supdeq_interpHRTF(hrtfDataset, denseSamplingGrid,'None','SH', nan, headRadius);
+interpHRTF_sh_timealign = supdeq_interpHRTF(hrtfDataset, denseSamplingGrid,'None','SH', nan, headRadius);
+interpHRTF_mca = supdeq_interpHRTF(hrtfDataset, denseSamplingGrid,'SUpDEq','SH', inf, headRadius);
 
 % Write SOFA file
-fs = 48000;
+fs = 44100;
 earDistance = 0.165;
 sourceDistance = 1.47;
+
+HRIR_L = permute(interpHRTF_sh.HRIR_L,[2 1]);
+HRIR_R = permute(interpHRTF_sh.HRIR_R,[2 1]);
+SOFAobj = supdeq_writeSOFAobj(HRIR_L, HRIR_R, denseSamplingGrid, fs, earDistance, sourceDistance); 
+SOFAsave('pp2_HRIRs_interpolated_sh', SOFAobj);
+
+HRIR_L = permute(interpHRTF_sh_timealign.HRIR_L,[2 1]);
+HRIR_R = permute(interpHRTF_sh_timealign.HRIR_R,[2 1]);
+SOFAobj = supdeq_writeSOFAobj(HRIR_L, HRIR_R, denseSamplingGrid, fs, earDistance, sourceDistance); 
+SOFAsave('pp2_HRIRs_interpolated_sh_timealign', SOFAobj);
+
 HRIR_L = permute(interpHRTF_mca.HRIR_L,[2 1]);
 HRIR_R = permute(interpHRTF_mca.HRIR_R,[2 1]);
 SOFAobj = supdeq_writeSOFAobj(HRIR_L, HRIR_R, denseSamplingGrid, fs, earDistance, sourceDistance); 
-
-% Save SOFA file
 SOFAsave('pp2_HRIRs_interpolated_mca', SOFAobj);
 
