46fedf5e82a6c8f64add7639056f998e4ff05bcc
mcht67 <malte.crt@googlemail.com>
2024-06-22 13:22:13 +0200

    fixes bug in hrtf interpolation

--- Matlab/MCA_Interpolation/MCAInterpolation.m
+++ Matlab/MCA_Interpolation/MCAInterpolation.m
@@ -1,11 +1,37 @@
+% COORDINATE STATES
+
+% 1. HUTUBS SOFA FILE (azim: [0, 360], elev [-90, 90])
+% 2. sparseHRIRdataset_SOFA (azim: [0, 360], elev [-90, 90]) [retrieved by sparseHRIRdataset.SourcePosition]
+% 3. hrtfDataset (azim: [0, 360], elev [0, 180]) [retrieved by hrtfDataset.samplingGrid]
+% 4. denseSamplingGrid (azim: [0, 360], elev [0, 180])
+% 5. interpHRTF (azim: [0, 360], elev [0, 180]) [retrieved by interpHRTF.samplingGrid]
+% 6. SOFAobj (azim: [0, 360], elev [-90, 90]) [retrieved by SOFAobj.SourcePosition]
+
+
+% CONCLUSIONS
+
+% supdeq_sofa2hrtf transforms elevation to colatitude
+% Code Snippet:
+% 0110     %Transform samplingGrid to SH coordinate system
+% 0111     samplingGrid(:,1) = mod(samplingGrid(:,1),360);
+% 0112     samplingGrid(:,2) = 90-samplingGrid(:,2);
+
+% supdeq_writeSOFAobj re-transforms colatitude to elevation
+% Code Snippet:
+% 0091 %Transform grid to SOFA format
+% 0092 samplingGrid(:,2) = 90-samplingGrid(:,2);
+
 % Get new sampling grid
 N = 44;
 denseSamplingGrid = supdeq_lebedev([], N);
-denseSamplingGrid(:,3) = [];
+
+% remove weights
+%denseSamplingGrid(:,3) = [1];
 
 % 1. import hrirs
 sparseHRIRdataset_SOFA = SOFAload('pp2_HRIRs_measured.sofa');
-hrtfDataset = supdeq_sofa2hrtf(sparseHRIRdataset_SOFA, N);
+hrtfDataset = supdeq_sofa2hrtf(sparseHRIRdataset_SOFA, 12);
+sparseSamplingGrid = hrtfDataset.samplingGrid;
 
 % MCA Interpolation
 headRadius = 0.0875;
@@ -19,9 +45,6 @@ interpHRTF_mca = supdeq_interpHRTF(hrtfDataset, denseSamplingGrid,'SUpDEq','SH',
 fs = 44100;
 earDistance = 0.165;
 sourceDistance = 1.47;
-
-% adjust sampling grid to SOFA azimuth (-90,90)
-%denseSamplingGrid(:,2) = denseSamplingGrid(:,2)-90;
 
 HRIR_L = permute(interpHRTF_sh.HRIR_L,[2 1]);
 HRIR_R = permute(interpHRTF_sh.HRIR_R,[2 1]);
